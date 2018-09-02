781612bfff761e565012dc89b491f814285e2a76
d_jingle <d_jingle@163.com>
2018-09-02 14:52:13 +0800

    1.0.0

--- src/login/login.vue
+++ src/login/login.vue
@@ -105,7 +105,7 @@
   }
 
 
-  .login{width:100%;height:100%;background:url('../../static/img/login-bg.png') no-repeat;background-size:cover;position:fixed;z-index:-10;}
+  .login{width:100%;height:100%;background:url('http://peert37b7.bkt.clouddn.com/login-bg.png') no-repeat;background-size:cover;position:fixed;z-index:-10;}
   .welcome{width:100%;margin:25% 0;}
   .welcome img{width:100%;}
   .login-inp{margin:0 30px 15px 30px;border:1px solid #fff;border-radius:25px;}
